5ee668098feb066788e57360da8fc45fea7ed547
Eduard <andreeveduard1@gmail.com>
2020-06-24 12:05:33 +0300

    Add Slider

--- js/script.js
+++ js/script.js
@@ -218,6 +218,57 @@ window.addEventListener('DOMContentLoaded', function () {
 		}
 	});
 
+	// Slider
 
-	
+	let slideIndex = 1, // параметр текущего слайда
+		slides = document.querySelectorAll('.slider-item'),
+		prev = document.querySelector('.prev'),
+		next = document.querySelector('.next'),
+		dotsWrap = document.querySelector('.slider-dots'),
+		dots = document.querySelectorAll('.dot');
+
+	showSlides(slideIndex);
+
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+
+		slides.forEach((item) => item.style.display = 'none');
+		// for (let i = 0; i < slides.length; i++) { // То же самое что метод forEach приведунный выше
+		//     slides[i].style.display = 'none';
+		// }
+		dots.forEach((item) => item.classList.remove('dot-active')); // удаляем класс
+
+		slides[slideIndex - 1].style.display = 'block';
+		dots[slideIndex - 1].classList.add('dot-active');
+	}
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) { // определяет текущий слайд и устанавливает его
+		showSlides(slideIndex = n);
+	}
+
+	prev.addEventListener('click', function () {
+		plusSlides(-1);
+	});
+	next.addEventListener('click', function () {
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function (event) { // делегирование событий
+		for (let i = 0; i < dots.length + 1; i++) { // не привязываемся к стилям только к элементам
+
+			if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+				// проверяем элемент на который кликнули есть ли у него класс dot
+				currentSlide(i);
+			}
+		}
+	});
 });
